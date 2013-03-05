2468d503b91e9012601e5521ed8ccc508bc635ff
Diullei Gomes <diullei@gmail.com>
2013-03-05 00:44:59 -0300

    updated tests

--- src/docEstract.ts
+++ src/docEstract.ts
@@ -1,8 +1,73 @@
 module d2t {
 
+    export class Tag {
+        name: string;
+        text: string;
+    }
+
     export class Comment {
         text: string;
-        constructor(public block: string) { }
+        tags: Tag[] = [];
+
+        constructor(public block: string) {
+            this.text = '';
+            var unwrapBlock = this.unwrap(block);
+            this.tags = this.splitTags(unwrapBlock);
+            var lines = unwrapBlock.match(/^.*((\r\n|\n|\r)|$)/gm);
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i];
+                if (this.isText(line)) {
+                    this.text += line + '\n';
+                }
+            }
+        }
+
+        isText(value: string) {
+            return !/^\s*@/.test(value);
+        }
+
+        unwrap(block: string) {
+            if (!block) {
+                return '';
+            }
+
+            return block.replace(/^\/\*\*+/, '') // remove opening slash+stars
+                        .replace(/\**\*\/$/, "\\Z")       // replace closing star slash with end-marker
+                        .replace(/^\s*(\* ?|\\Z)/gm, '')  // remove left margin like: spaces+star or spaces+end-marker
+                        .replace(/\s*\\Z$/g, '');
+        }
+
+        splitTags(block: string): Tag[] {
+            var tags: Tag[] = [];
+            var tagText: string;
+            var tagTitle: string;
+
+            // split out the basic tags, keep surrounding whitespace
+            // like: @tagTitle tagBody
+            block
+            .replace(/^(\s*)@(\S)/gm, '$1\\@$2') // replace splitter ats with an arbitrary sequence
+            .split('\\@')                        // then split on that arbitrary sequence
+            .forEach(function ($) {
+                if ($) {
+                    var parsedTag = $.match(/^(\S+)(:?\s+(\S[\s\S]*))?/);
+
+                    if (parsedTag) {
+                        // we don't need parsedTag[0]
+                        tagTitle = parsedTag[1];
+                        tagText = parsedTag[2];
+
+                        if (tagTitle) {
+                            tags.push(<Tag>{
+                                name: tagTitle,
+                                text: tagText
+                            });
+                        }
+                    }
+                }
+            });
+
+            return tags;
+        }
     }
 
     export class Parser {
@@ -21,9 +86,7 @@ module d2t {
 
             while ((result = reg.exec(text)) !== null) {
                 var match = result[0];
-
                 comments.push(new Comment(match));
-
                 text = text.substr(result[0].index);
                 c += match.length;
             }
--- test/docEstract-test.ts
+++ test/docEstract-test.ts
@@ -14,17 +14,23 @@ QUnit.test("get amount of comments", function () {
 
 QUnit.test("extract comment block", function () {
     var comments: d2t.Comment[] = new d2t.Parser(fs.readFileSync("test/fixtures/comment.js", 'utf8')).exec();
-    QUnit.equal('/**\n\r\
-* My property description. Like other pieces of your comment blocks,\n\r\
-* this can span multiple lines.\n\r\
-*\n\r\
-* @property propertyName\n\r\
-* @type {Object}\n\r\
-* @default "foo"\n\r\
+    QUnit.equal('/**\r\n\
+* My property description. Like other pieces of your comment blocks, \r\n\
+* this can span multiple lines.\r\n* \r\n\
+* @property propertyName\r\n\
+* @type {Object}\r\n\
+* @default "foo"\r\n\
 */', comments[2].block);
 });
 
 QUnit.test("extract comment text", function () {
     var comments: d2t.Comment[] = new d2t.Parser(fs.readFileSync("test/fixtures/comment.js", 'utf8')).exec();
-    QUnit.equal("My method description.  Like other pieces of your comment blocks, \nthis can span multiple lines.", comments[0].text);
+    QUnit.equal('My method description. Like other pieces of your comment blocks, \n\
+this can span multiple lines.', comments[1].text);
+});
+
+QUnit.test("extract tags", function () {
+    var comments: d2t.Comment[] = new d2t.Parser(fs.readFileSync("test/fixtures/comment.js", 'utf8')).exec();
+    QUnit.equal('param', comments[1].tags[2].name);
+    QUnit.equal('{Object} config A config object', comments[1].tags[2].text);
 });
